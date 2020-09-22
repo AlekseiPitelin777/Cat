2070833893ea9c0515eeb7bd0329e94dafbe8d3d
pitelinalexey <pitelinalexey@mail.ru>
2020-09-22 14:23:00 +0300

    cats created and their dead

--- Cat/src/Loader.java
+++ Cat/src/Loader.java
@@ -2,34 +2,23 @@ import java.util.concurrent.Callable;
 
 public class Loader
 {
-    public static void main(String[] args)
-    {
-        Cat John = new Cat();
-        John.meow();
-        System.out.println("John weight:" + John.getWeight());
-        //==========================================================
+    public static void main(String[] args) {
         Cat Patrick = new Cat();
-        System.out.println("Patrick weight:" + Patrick.getWeight());
-        Patrick.feed(56.6);
-        System.out.println("Patrick weight:" + Patrick.getWeight());
-        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        Cat Wilson = new Cat();
-        Wilson.drink(105.65);
-        System.out.println("Wilson weight:" + Wilson.getWeight());
-        //----------------------------------------------------------
-        Cat Stiv = new Cat();
-        System.out.println("Stiv weight:" + Stiv.getWeight());
-        Stiv.feed(98.5);
-        System.out.println("Stiv weight:" + Stiv.getWeight());
-        //***********************************************************
-        Cat Kevin = new Cat();
-        System.out.println("Kevin weight:" + Kevin.getWeight());
-        while (Kevin.getStatus() =  {
-            Kevin.feed(500.5);
+        System.out.println ( "Patrick - " + Patrick.getWeight());
+        Patrick.feed(450.45);
+        System.out.println("Patrick - " + Patrick.getWeight());
+       //================================================
+        Cat John = new Cat();
+        while (John.getStatus() != "Dead") {
+            John.meow();
         }
-
-
-
+        System.out.println("John - " + John.getStatus());
+        //======================================
+        Cat Wilson = new Cat();
+        while (Wilson.getStatus() != "Exploded") {
+            Wilson.feed(100.4);
+        }
+        System.out.println( "Wilson - " + Wilson.getStatus());
 
 
     }
